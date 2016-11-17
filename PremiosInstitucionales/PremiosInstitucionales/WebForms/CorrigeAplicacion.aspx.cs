b65b483129a6a6770abc43cae62daacd8499b581
Cesar <cesars90_@hotmail.com>
2016-11-17 15:27:25 -0600

    Creado web form CorrigeAplicacion

---
+++ PremiosInstitucionales/PremiosInstitucionales/WebForms/CorrigeAplicacion.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace PremiosInstitucionales.WebForms
+{
+    public partial class CorrigeAplicacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // crear botones de prueba
+            Button btn1 = new Button();
+            btn1.ID = "btn1";
+            btn1.Text = "abrir popup";
+            btn1.OnClientClick = "return ShowModalPopup(\"abc\")";
+            panel.Controls.Add(btn1);
+        }
+
+        protected void bttnEnviarRechazo_Click(object sender, EventArgs e)
+        {
+            String s = IdAppHidden.Value.ToString();
+            // USAR EL VALOR DEL STRING
+        }
+    }
+}
